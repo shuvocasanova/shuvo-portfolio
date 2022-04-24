48152dc1478d0836d94d0ec75dede62d2df36e93
shuvocasanova <shuvocasanova@gmail.com>
2022-04-24 18:48:53 +0600

    NavBar completed

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-
+import React, { useState} from 'react';
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import {motion} from 'framer-motion';
+ 
+import { images } from "../../constants";
 import './Navbar.scss';
 
 const Navbar = () => {
+  const [toogle, setToogle] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt="logo" />
+        </div>
+        <ul className='app__navbar-links'>
+          {['home', 'about', 'skills', 'work', 'contact'].map((item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className='app__navbar-menu'>
+            <HiMenuAlt4 onClick={() => setToogle(true)} />
+
+            {
+              toogle && (
+                <motion.div
+                  whileInView={{ x: [300, 0]}}
+                  transition={{ duration: 0.85, ease: 'easeOut'}}
+                >
+                  <HiX onClick={() => setToogle(false)} />
+                    <ul >
+                    {['home', 'about', 'skills', 'work', 'contact'].map((item) => (
+                    <li key={item}>
+                      <a href={`#${item}`} onClick={() => setToogle(false)} >{item}</a>
+                    </li>
+                   ))}
+                   </ul>
+                </motion.div>
+              )}
+        </div>
+    </nav>
+  );
+};
 
 export default Navbar
